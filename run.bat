8d2153a572bf82b88362feeb87540782b9fd2997
ĐØ₵₮ØⱤ ⱧɆ₦₮₳ł <96329821+IamNotDaniil@users.noreply.github.com>
2026-06-03 10:37:55 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,18 @@
 @echo off
-echo Starting SC2 Fog Remover...
-echo Make sure StarCraft II is running!
+title SC2 Fog Remover
+echo ========================================
+echo    SC2 Fog of War Remover v2.0
+echo ========================================
 echo.
-cd bin\Release\net8.0
-SC2_FogRemover.exe
+echo 1. Make sure StarCraft II is running
+echo 2. This program requires Administrator rights
+echo.
 pause
+cd /d "%~dp0bin\Release\net8.0-windows"
+if exist "SC2_FogRemover.exe" (
+    powershell -Command "Start-Process -FilePath 'SC2_FogRemover.exe' -Verb RunAs -WorkingDirectory '%~dp0bin\Release\net8.0-windows'"
+) else (
+    echo [ERROR] SC2_FogRemover.exe not found!
+    echo Please run build.bat first.
+    pause
+)
